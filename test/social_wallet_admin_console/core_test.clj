d165351069b0e8c1b8c5fd9556726b2e111ca49e
Aspasia Beneti <aspasia.beneti@gmail.com>
2018-01-11 13:49:58 +0100

    Starting the gorilla repo via a test

--- test/social_wallet_admin_console/core_test.clj
+++ test/social_wallet_admin_console/core_test.clj
@@ -1,7 +1,13 @@
 (ns social-wallet-admin-console.core-test
   (:require [clojure.test :refer :all]
-            [social-wallet-admin-console.core :refer :all]))
+            [social-wallet-admin-console.core :refer :all]
+            [gorilla-repl.core :as gc]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest run-gorilla-server
+  (gc/run-gorilla-server {:port 8990}))
+
+
